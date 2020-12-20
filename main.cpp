cbd863bda62d628b5fbfa248b454ca4ef9bac9ca
huoliang92 <huolianghfut@163.com>
2020-12-20 14:01:11 +0800

    可以在VScode上编译C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+#include <iostream>
 int main()
 {
-    using namespace std ;
-    cout<<"666"<<endl;
+    using namespace std;
+    cout << "Enter for add " << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+    cin.get();
     return 0;
 }
